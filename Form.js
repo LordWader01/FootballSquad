6896553a8a1ad8fdb1cb1b79628c5ce069cb3c94
Your NameMonk <foszek65@gmail.com>
2019-01-16 23:22:44 +0100

    Pewien moment projektu

--- Form.js
+++ Form.js
@@ -1,1 +1,18 @@
+let inputs = document.querySelectorAll('input');
+let isActive = function(element){
+    return document.activeElement === element;
+}
 
+inputs.addEventListener('keypress', function (e) {
+        var key = e.which || e.keyCode;
+        if (key === 13) { // 13 is enter, check if user clicked enter
+        // code for enter
+            for (const inpt of this) {
+                if (isActive(inpt)) {
+                    let inptVal = inpt.value;
+                    inpt.style.display = "none";
+                    inpt.parentElement.innerHTML = inptVal;
+                }
+            }
+        }
+});
